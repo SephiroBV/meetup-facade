8039e699c31ac0d3cdda4f3252e073eee3ce3601
tiernan <tiernan@sephiro.nl>
2022-04-06 14:34:52 +0200

    minor refactor

--- src/main.rs
+++ src/main.rs
@@ -17,14 +17,17 @@ async fn main() -> std::io::Result<()> {
     let server = if let Some(listener) = ListenFd::from_env().take_tcp_listener(0)? {
         HttpServer::new(app_factory).listen(listener)?
     } else {
-        //Google Cloud Run Port Injection
-        let address = match std::env::var("PORT") {
-            Ok(port) => format!("0.0.0.0:{}", port),
-            Err(_e) => "127.0.0.1:8000".into(),
-        };
-        HttpServer::new(app_factory).bind(address)?
+        HttpServer::new(app_factory).bind(get_configured_address())?
     };
-    tracing::info!("Starting app, listening on {:?}", server.addrs());
 
+    tracing::info!("Starting app, listening on {:?}", server.addrs());
     server.run().await
+}
+
+///Google Cloud Run Port Injection
+fn get_configured_address() -> String {
+    match std::env::var("PORT") {
+        Ok(port) => format!("0.0.0.0:{}", port),
+        Err(_e) => "127.0.0.1:8000".into(),
+    }
 }
